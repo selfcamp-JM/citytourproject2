60a918c6e9ae5d5b6158627b884d1f09ba5acefa
selfcamp-JM <jakhongir.mamadov@gmail.com>
2019-01-15 02:03:59 +0300

    Created components and added scss files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
+import Navbar from "./components/Navbar/Navbar";
+import Tourlist from "./components/Tourlist/index";
 
 class App extends Component {
   render() {
     return (
+    <div>
+      <Navbar/>
       <h1>Hello</h1>
+      <Tourlist/>
+      
+    </div>
     );
   }
 }
---
+++ src/components/Tourlist/index.js
@@ -0,0 +1,14 @@
+import React,{Component} from  "react";
+
+
+
+export default class Tourlist extends Component {
+ render(){
+     return(
+         <div>
+             Hello tourlist
+         </div>
+     )
+ }
+
+}
